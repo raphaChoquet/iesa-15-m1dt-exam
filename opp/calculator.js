75ee1300bb35c2ac77b5164cd0ab9ea76c37207b
Raphael <rchoquet@iesanetwork.com>
2015-03-25 12:03:47 +0100

    add exception in calcultator.add

--- opp/calculator.js
+++ opp/calculator.js
@@ -5,7 +5,16 @@ function Calculator ()
 
 Calculator.prototype = {
 	add: function (nbr) {
+		if(this.add.arguments.length != 1) {
+			throw "Accepte only one argument";
+		}
+
+		nbr = Number(nbr);
+		if (isNaN(nbr)) {
+			throw "Accepte only number";
+		} 
 		this.result += parseInt(nbr);
+
 	},
 	minus: function (nbr) {
 
