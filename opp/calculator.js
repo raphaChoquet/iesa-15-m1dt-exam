24ce260eebb3cd55a1cffe7bf87b355686680337
Raphael <rchoquet@iesanetwork.com>
2015-03-25 10:52:39 +0100

    Mise en place des fichiers et de la structure de Calculator.js

---
+++ opp/calculator.js
@@ -0,0 +1,19 @@
+function Calculator ()
+{
+	this.result = 0;
+}
+
+Calculator.prototype = {
+	add: function (nbr) {
+
+	},
+	minus: function (nbr) {
+
+	},
+	divide: function (nbr) {
+
+	}, 
+	multiply: function (nbr) {
+
+	}
+}
